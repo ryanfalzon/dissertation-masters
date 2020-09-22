4c34139e664111cc9d0b8bc07b4cdc5fab22bf8a
Ryan Falzon <musicfreakzryan@hotmail.com>
2020-09-22 14:29:33 +0200

    Created Ethereum connector.

---
+++ Dissertation/src/UnifiedModel/UnifiedModel.Connectors/Ethereum/Connector.cs
@@ -0,0 +1,30 @@
+﻿using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+
+namespace UnifiedModel.Connectors.Ethereum
+{
+    public class Connector
+    {
+        private readonly Web3 web3;
+        private readonly string publicKey;
+        private readonly string abi;
+        private readonly string contractAddress;
+
+        public Connector(string publicKey, string privateKey)
+        {
+            web3 = new Web3(new Account(privateKey));
+        }
+
+        public TransactionReceipt Call(string functionName, params object[] functionInput)
+        {
+            var contract = web3.Eth.GetContract(abi, contractAddress);
+            var function = contract.GetFunction(functionName);
+
+            var gas = function.EstimateGasAsync(publicKey, null, null, functionInput).Result;
+            var result = function.SendTransactionAndWaitForReceiptAsync(publicKey, gas, null, null, functionInput: functionInput).Result;
+
+            return result;
+        }
+    }
+}
---
+++ Dissertation/src/UnifiedModel/UnifiedModel.Connectors/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UnifiedModel.Connectors
+{
+    public static class Extensions
+    {
+        public static string Hash(this object @object)
+        {
+            using SHA256 sha256Hash = SHA256.Create();
+            return sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@object))).ToHexString();
+        }
+
+        public static string ToHexString(this byte[] data)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                stringBuilder.Append(data[i].ToString("x2"));
+            }
+            return stringBuilder.ToString();
+        }
+
+        public static void Assert(bool assertion)
+        {
+            if (!assertion)
+            {
+                throw new InvalidDataException("Null values passed as parameters...");
+            }
+        }
+
+        public static bool IsNotNull(this object @object)
+        {
+            foreach (PropertyInfo propertyInfo in @object.GetType().GetProperties())
+            {
+                var value = propertyInfo.GetValue(@object);
+                if(value == null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
